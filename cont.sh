aaeb8b09c3c84dbf4e7f3a0146706225de70b7f9
Spencer Baugh <sbaugh@catern.com>
2018-03-11 15:09:00 -0400

    craziness

--- cont.sh
+++ cont.sh
@@ -39,11 +39,6 @@ function file_wait_for_one_line() {
     proc_monitor $proc tail --bytes=+$bytecount --follow $file | { head -n1; proc_kill $proc; }
 }
 
-function msg_send() {
-    file=$1; shift
-    echo "$@" >> $file
-}
-
 # raise and continue should be separate
 # this way only allows one continuation to be blocked at a prompt at a time.
 # we can't switch between continuations!
@@ -54,23 +49,40 @@ function msg_send() {
 # and maybe msg_call also should be broken up?
 
 # the continuation file changes each time we yield.
-
-function msg_call() {
-    prompt=$1; shift
-    continue_size=$(file_size $prompt/continue)
-    msg_send $prompt/raise "$@"
-    file_wait_for_one_line $prompt/continue $continue_size
+function msg_new() {
+    template=$1; shift
+    msgqueue=$(name continuation.XXXX)
+    touch $msgqueue
+    echo $msgqueue
 }
 
-function msg_receive() {
-    prompt=$1; shift
-    raise_size=$(file_size $prompt/raise)
-    file_wait_for_one_line $prompt/raise $raise_size
+function msg_send() {
+    file=$1; shift
+    echo "$@" >> $file
 }
 
-function msg_reply() {
+function msg_receive_once() {
+    msgqueue=$1; shift
+    file_wait_for_one_line $msgqueue 0
+}
+
+function msg_receive_new() {
+    msgqueue=$1; shift
+    size=$1; shift
+    file_wait_for_one_line $msgqueue $size
+}
+
+function msg_old_marker() {
+    msgqueue=$1; shift
+    file_size $msgqueue
+}
+
+function run_something() {
     prompt=$1; shift
-    msg_send $prompt/continue "$@"
+    prompt_old_marker=$(msg_old_marker $prompt)
+    msg_send
+    # do thing
+    msg_receive_new $prompt $prompt_old_marker
 }
 
 function make_prompt() {
@@ -85,7 +97,12 @@ function wrap_return() {
     prompt=$1; shift
     stdout=$(name stdout.XXXX)
     "$@" $prompt > $stdout
-    msg_send $prompt/raise "return: $stdout"
+    msg_send $prompt "return: $stdout"
+}
+
+function push_subcont() {
+    prompt=$1; shift
+    continuation=$1; shift
 }
 
 function run() {
@@ -96,14 +113,19 @@ function run() {
     proc=$(proc_create)
     prompt=$(make_prompt)
     proc_monitor $proc wrap_return $prompt "$@" &
+    # clean this guy up somehow??
+    # clean up the running process, I mean
+    # no just leave him around
+    
     tail -c+0 -f $prompt/raise
 }
 
 function yield() {
     prompt=$1; shift
-    msg_call $prompt "$@"
+    continuation=$(msg_new continuation.XXXX)
+    msg_send $prompt "yield: $continuation" "$@"
+    msg_receive_once $continuation
 }
-
 
 file=$1; shift
 size=$(file_size $file)
